59d60425dc9e2623b2c68c70a0ef43a4a46cc2d5
Vesa-Pekka Palmu <vpalmu@depili.fi>
2019-01-17 20:55:05 +0200

    Implement SDL display for the clock
    Defaults to 1080p mode, use -s for 192x192 display.

---
+++ cmd/sdl-clock/data.go
@@ -0,0 +1,187 @@
+package main
+
+var winTitle string = "SDL CLOCK"
+var winWidth, winHeight int32 = 1920, 1080
+var gridStartX int32 = 569
+var gridStartY int32 = 149
+var gridSize int32 = 20
+var gridSpacing int = 25
+
+var Options struct {
+	Small       bool   `short:"s" description:"Scale to 192x192px"`
+	Font        string `short:"F" long:"font" description:"Font for event name" default:"fonts/7x13.bdf"`
+	TextRed     uint8  `short:"r" long:"red" description:"Red component of text color" default:"255"`
+	TextGreen   uint8  `short:"g" long:"green" description:"Green component of text color" default:"128"`
+	TextBlue    uint8  `short:"b" long:"blue" description:"Blue component of text color" default:"0"`
+	StaticRed   uint8  `long:"static-red" description:"Red component of static color" default:"80"`
+	StaticGreen uint8  `long:"static-green" description:"Green component of static color" default:"80"`
+	StaticBlue  uint8  `long:"static-blue" description:"Blue component of static color" default:"0"`
+	SecRed      uint8  `long:"sec-red" description:"Red component of second color" default:"200"`
+	SecGreen    uint8  `long:"sec-green" description:"Green component of second color" default:"0"`
+	SecBlue     uint8  `long:"sec-blue" description:"Blue component of second color" default:"0"`
+	LocalTime   string `short:"t" long:"local-time" description:"Local timezone" default:"Europe/Helsinki"`
+	ForeignTime string `short:"T" long:"foreign-time" description:"Foreign timezone" default:"Europe/Moscow"`
+	TimePin     int    `short:"p" long:"time-pin" description:"Pin to select foreign timezone, active low" default:"15"`
+	ListenAddr  string `long:"osc-listen" description:"Address to listen for incoming osc messages" required:"true"`
+	Timeout     int    `short:"d" long:"timeout" description:"Timeout for OSC message updates in milliseconds" default:"1000"`
+}
+
+// 12 "Hour" static circles
+var staticCircles [12][2]int32 = [12][2]int32{
+	{1393, 790},
+	{1210, 973},
+	{960, 1040},
+	{710, 973},
+	{526, 790},
+	{460, 540},
+	{526, 290},
+	{709, 106},
+	{959, 40},
+	{1210, 106},
+	{1393, 289},
+	{1460, 539},
+}
+
+// radius 500 * 192 / 1080
+var smallStaticCircles [12][2]int32 = [12][2]int32{
+	{141, 172},
+	{173, 140},
+	{183, 96},
+	{171, 52},
+	{138, 19},
+	{93, 8},
+	{50, 19},
+	{18, 51},
+	{8, 95},
+	{20, 140},
+	{53, 172},
+	{98, 184},
+}
+
+// Second circles
+var secCircles [60][2]int32 = [60][2]int32{
+	{959, 90},
+	{1007, 92},
+	{1053, 99},
+	{1099, 112},
+	{1143, 128},
+	{1185, 150},
+	{1224, 175},
+	{1261, 205},
+	{1294, 238},
+	{1324, 275},
+	{1349, 315},
+	{1371, 356},
+	{1387, 400},
+	{1400, 446},
+	{1407, 492},
+	{1410, 539},
+	{1407, 587},
+	{1400, 633},
+	{1387, 679},
+	{1371, 723},
+	{1349, 765},
+	{1324, 804},
+	{1294, 841},
+	{1261, 874},
+	{1224, 904},
+	{1185, 929},
+	{1143, 951},
+	{1099, 967},
+	{1053, 980},
+	{1007, 987},
+	{960, 990},
+	{912, 987},
+	{866, 980},
+	{820, 967},
+	{776, 951},
+	{735, 929},
+	{695, 904},
+	{658, 874},
+	{625, 841},
+	{595, 804},
+	{570, 765},
+	{548, 723},
+	{532, 679},
+	{519, 633},
+	{512, 587},
+	{510, 540},
+	{512, 492},
+	{519, 446},
+	{532, 400},
+	{548, 356},
+	{570, 315},
+	{595, 275},
+	{625, 238},
+	{658, 205},
+	{695, 175},
+	{734, 150},
+	{776, 128},
+	{820, 112},
+	{866, 99},
+	{912, 92},
+}
+
+// n = 60, center = 192 / 2, radius = 450 * 192 / 1080
+var smallSecCircles [60][2]int32 = [60][2]int32{
+	{94, 16},
+	{102, 16},
+	{110, 17},
+	{118, 19},
+	{126, 22},
+	{134, 26},
+	{141, 31},
+	{148, 36},
+	{154, 42},
+	{159, 48},
+	{164, 56},
+	{168, 63},
+	{171, 71},
+	{173, 79},
+	{175, 87},
+	{175, 96},
+	{175, 104},
+	{174, 112},
+	{172, 120},
+	{169, 128},
+	{166, 136},
+	{161, 143},
+	{156, 149},
+	{150, 155},
+	{144, 160},
+	{137, 165},
+	{130, 169},
+	{122, 172},
+	{114, 174},
+	{106, 175},
+	{97, 176},
+	{89, 175},
+	{81, 174},
+	{73, 172},
+	{65, 169},
+	{57, 165},
+	{50, 160},
+	{43, 155},
+	{37, 149},
+	{32, 143},
+	{27, 136},
+	{23, 128},
+	{20, 120},
+	{18, 112},
+	{16, 104},
+	{16, 95},
+	{16, 87},
+	{17, 79},
+	{19, 71},
+	{22, 63},
+	{25, 55},
+	{30, 48},
+	{35, 42},
+	{41, 36},
+	{47, 31},
+	{54, 26},
+	{61, 22},
+	{69, 19},
+	{77, 17},
+	{85, 16},
+}
---
+++ cmd/sdl-clock/main.go
@@ -0,0 +1,289 @@
+package main
+
+import (
+	"fmt"
+	"github.com/SpComb/osc-tally/clock"
+	"github.com/depili/go-rgb-led-matrix/bdf"
+	// "github.com/depili/go-rgb-led-matrix/matrix"
+	"github.com/desertbit/timer"
+	"github.com/hypebeast/go-osc/osc"
+	"github.com/jessevdk/go-flags"
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi" // This loads the RPi driver
+	"github.com/veandco/go-sdl2/gfx"
+	"github.com/veandco/go-sdl2/sdl"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var staticSDLColor sdl.Color = sdl.Color{80, 80, 0, 255} // 12 static indicator circles
+var secSDLColor sdl.Color = sdl.Color{200, 0, 0, 255}
+var textSDLColor sdl.Color
+var window *sdl.Window
+var renderer *sdl.Renderer
+var parser = flags.NewParser(&Options, flags.Default)
+
+var textureSource sdl.Rect
+var staticTexture *sdl.Texture
+var secTexture *sdl.Texture
+
+func runOSC(oscServer *osc.Server) {
+	err := oscServer.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	if _, err := parser.Parse(); err != nil {
+		panic(err)
+	}
+
+	// Initialize osc listener
+	var oscServer = osc.Server{
+		Addr: Options.ListenAddr,
+	}
+
+	var clockServer = clock.MakeServer(&oscServer)
+	log.Printf("osc server: listen %v", oscServer.Addr)
+
+	go runOSC(&oscServer)
+
+	// GPIO pin for toggling between timezones
+	if err := embd.InitGPIO(); err != nil {
+		panic(err)
+	}
+
+	timePin, err := embd.NewDigitalPin(Options.TimePin)
+	if err != nil {
+		panic(err)
+	} else if err := timePin.SetDirection(embd.In); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("GPIO initialized.\n")
+
+	// Load timezones
+	local, err := time.LoadLocation(Options.LocalTime)
+	if err != nil {
+		panic(err)
+	}
+
+	foreign, err := time.LoadLocation(Options.ForeignTime)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Timezones loaded.\n")
+
+	// Parse font for clock text
+	font, err := bdf.Parse(Options.Font)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Fonts loaded.\n")
+
+	// Initialize SDL
+
+	if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to initialize SDL: %s\n", err)
+		return
+	}
+	defer sdl.Quit()
+
+	if window, err = sdl.CreateWindow(winTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, winWidth, winHeight, sdl.WINDOW_SHOWN); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create window: %s\n", err)
+		return
+	}
+	defer window.Destroy()
+
+	if renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create renderer: %s\n", err)
+		return // don't use os.Exit(3); otherwise, previous deferred calls will never run
+	}
+
+	sdl.ShowCursor(0) // Hide mouse cursor
+
+	renderer.Clear()
+	defer renderer.Destroy()
+
+	fmt.Printf("SDL init done\n")
+
+	rendererInfo, _ := renderer.GetInfo()
+	fmt.Printf("Renderer: %v\n", rendererInfo.Name)
+
+	// Clock colors from flags
+	textSDLColor = sdl.Color{Options.TextRed, Options.TextGreen, Options.TextBlue, 255}
+	staticSDLColor = sdl.Color{Options.StaticRed, Options.StaticGreen, Options.StaticBlue, 255}
+	secSDLColor = sdl.Color{Options.SecRed, Options.SecGreen, Options.SecBlue, 255}
+	// Default color for the OSC field (black)
+	tallyColor := sdl.Color{0, 0, 0, 255}
+
+	var textureSize int32 = 40
+	var textureCoord int32 = 20
+	var textureRadius int32 = 19
+
+	// Create a texture for circles
+	if Options.Small {
+		textureSize = 8
+		textureCoord = 3
+		textureRadius = 3
+		gridStartX = 32
+		gridStartY = 32
+		gridSize = 3
+		gridSpacing = 4
+		secCircles = smallSecCircles
+		staticCircles = smallStaticCircles
+	}
+	staticTexture, _ = renderer.CreateTexture(sdl.PIXELFORMAT_RGBA8888, sdl.TEXTUREACCESS_TARGET, textureSize, textureSize)
+	renderer.SetRenderTarget(staticTexture)
+	gfx.FilledCircleColor(renderer, textureCoord, textureCoord, textureRadius, staticSDLColor)
+	if !Options.Small {
+		gfx.AACircleColor(renderer, textureCoord, textureCoord, textureRadius, staticSDLColor)
+	}
+
+	secTexture, _ = renderer.CreateTexture(sdl.PIXELFORMAT_RGBA8888, sdl.TEXTUREACCESS_TARGET, textureSize, textureSize)
+	renderer.SetRenderTarget(secTexture)
+	gfx.FilledCircleColor(renderer, textureCoord, textureCoord, textureRadius, secSDLColor)
+	if !Options.Small {
+		gfx.AACircleColor(renderer, textureCoord, textureCoord, textureRadius, secSDLColor)
+	}
+
+	renderer.SetRenderTarget(nil)
+	textureSource = sdl.Rect{0, 0, textureSize, textureSize}
+
+	// Trap SIGINT aka Ctrl-C
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+
+	// Create initial bitmaps for clock text
+	hourBitmap := font.TextBitmap("15")
+	minuteBitmap := font.TextBitmap("04")
+	secondBitmap := font.TextBitmap("05")
+	tallyBitmap := font.TextBitmap("  ")
+
+	updateTicker := time.NewTicker(time.Millisecond * 30)
+	oscChan := clockServer.Listen()
+
+	timeout := timer.NewTimer(time.Duration(Options.Timeout) * time.Millisecond)
+
+	fmt.Printf("Entering main loop\n")
+
+	for {
+		select {
+		case msg := <-oscChan:
+			// New OSC message received
+			fmt.Printf("Got new osc data.\n")
+			tallyColor = sdl.Color{uint8(msg.ColorRed), uint8(msg.ColorGreen), uint8(msg.ColorBlue), 255}
+			tallyBitmap = font.TextBitmap(fmt.Sprintf("%1s%02d%1s", msg.Symbol, msg.Count, msg.Unit))
+			timeout.Reset(time.Duration(Options.Timeout) * time.Millisecond)
+		case <-timeout.C:
+			// OSC message timeout
+			tallyBitmap = font.TextBitmap("")
+		case <-sigChan:
+			// SIGINT received, shutdown gracefully
+			os.Exit(1)
+		case <-updateTicker.C:
+			// Update the time shown on the screen
+			var t time.Time
+			if i, _ := timePin.Read(); err != nil {
+				panic(err)
+			} else if i == 1 {
+				t = time.Now().In(local)
+			} else {
+				t = time.Now().In(foreign)
+			}
+
+			// Check that the rpi has valid time
+			if t.Year() > 2000 {
+				hourBitmap = font.TextBitmap(t.Format("15"))
+				minuteBitmap = font.TextBitmap(t.Format("04"))
+				secondBitmap = font.TextBitmap(t.Format("05"))
+			} else {
+				// No valid time, indicate it with "XX" as the time
+				hourBitmap = font.TextBitmap("XX")
+				minuteBitmap = font.TextBitmap("XX")
+				secondBitmap = font.TextBitmap("")
+			}
+			seconds := t.Second()
+
+			// Clear SDL canvas
+			renderer.SetDrawColor(0, 0, 0, 255) // Black
+			renderer.Clear()                    // Clear screen
+
+			// Dots between hours and minutes
+			drawDots()
+
+			// Draw the text
+			drawBitmask(hourBitmap, textSDLColor, 10, 0)
+			drawBitmask(minuteBitmap, textSDLColor, 10, 17)
+			drawBitmask(secondBitmap, textSDLColor, 21, 8)
+			drawBitmask(tallyBitmap, tallyColor, 0, 2)
+
+			drawStaticCircles()
+			drawSecondCircles(seconds)
+
+			// Update the canvas
+			renderer.Present()
+		}
+	}
+}
+
+func drawSecondCircles(seconds int) {
+	// Draw second circles
+	for i := 0; i <= int(seconds); i++ {
+		// gfx.FilledCircleColor(renderer, secCircles[i][0], secCircles[i][1], 20, secSDLColor)
+		dest := sdl.Rect{secCircles[i][0] - 20, secCircles[i][1] - 20, 40, 40}
+		if Options.Small {
+			dest = sdl.Rect{secCircles[i][0] - 4, secCircles[i][1] - 4, 8, 8}
+		}
+		renderer.Copy(secTexture, &textureSource, &dest)
+	}
+}
+
+func drawStaticCircles() {
+	// Draw static indicator circles
+	for _, p := range staticCircles {
+		if Options.Small {
+			dest := sdl.Rect{p[0] - 4, p[1] - 4, 8, 8}
+			renderer.Copy(staticTexture, &textureSource, &dest)
+		} else {
+			dest := sdl.Rect{p[0] - 20, p[1] - 20, 40, 40}
+			renderer.Copy(staticTexture, &textureSource, &dest)
+		}
+	}
+}
+
+func drawDots() {
+	// Draw the dots between hours and minutes
+	setPixel(14, 15, textSDLColor)
+	setPixel(14, 16, textSDLColor)
+	setPixel(15, 15, textSDLColor)
+	setPixel(15, 16, textSDLColor)
+
+	setPixel(18, 15, textSDLColor)
+	setPixel(18, 16, textSDLColor)
+	setPixel(19, 15, textSDLColor)
+	setPixel(19, 16, textSDLColor)
+}
+
+// Set "led matrix" pixel
+func setPixel(cy, cx int, color sdl.Color) {
+	x := gridStartX + int32(cx*gridSpacing)
+	y := gridStartY + int32(cy*gridSpacing)
+	rect := sdl.Rect{x, y, gridSize, gridSize}
+	renderer.SetDrawColor(color.R, color.G, color.B, color.A)
+	renderer.FillRect(&rect)
+}
+
+func drawBitmask(bitmask [][]bool, color sdl.Color, r int, c int) {
+	for y, row := range bitmask {
+		for x, b := range row {
+			if b {
+				setPixel(r+y, c+x, color)
+			}
+		}
+	}
+}
