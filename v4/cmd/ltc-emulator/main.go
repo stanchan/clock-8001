6973aa8f7e62bc020436542d9988a17e41f4aa66
Vesa-Pekka Palmu <vpalmu@depili.fi>
2021-02-09 18:46:55 +0200

    Quick and dirty test tool to send LTC timecode OSC messages

---
+++ v4/cmd/ltc-emulator/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hypebeast/go-osc/osc"
+	"time"
+)
+
+func main() {
+	client := osc.NewClient("255.255.255.255", 1245)
+	frames := 0
+	for {
+		msg := osc.NewMessage("/clock/ltc")
+		t := time.Now().Format("03:04:05")
+		text := fmt.Sprintf("%s:%02d", t, frames)
+		msg.Append(text)
+		client.Send(msg)
+		frames++
+		frames = frames % 60
+		time.Sleep(time.Second / 30)
+	}
+}
